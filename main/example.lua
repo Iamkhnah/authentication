dd7283c38d11f38fb53da89320c83f62f78317ee
ItOnltz <hohohubbro@gmail.com>
2025-05-28 12:36:10 +0700

    Create example.lua

---
+++ main/example.lua
@@ -0,0 +1,10 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iamkhnah/authentication/refs/heads/main/main/script.luau"))()
+lib:Start({
+    ["Title"] = "Mushroom Authentication", -- your name script
+    ["Discord"] = "https://discord.gg/Cp9kuVGrce", -- your link invite discord
+    ["Api Key"] = "68369fca750d2d2d516cfcc4", -- link4m
+    ["Script"] = function()
+        print("it only execute if user getkey")
+        --[[ You can put a script here for ex: loadstring(), or raw code ]]
+    end
+})
